ade0cdfb69efd657f6c86fec9432ef171808b6ee
ArtemDaemon <artem.daemon.official@gmail.com>
2024-09-29 12:14:01 +0700

    Допичнил

---
+++ src/FA.java
@@ -0,0 +1,53 @@
+public class FA {
+    private enum STATES {q0, q1, q2, q3, q4, q5, q6}
+    private static final STATES[] ACCEPTED_STATES = {STATES.q5};
+    private enum INPUT {_0, _1}
+    private static final char[] ALPHABET = {'0', '1'};
+
+    private static final int TOTAL_STATES = STATES.values().length;
+    private static final int ALPHABET_CHARACTERS = ALPHABET.length;
+    private static final int[][] transitionTable = new int[TOTAL_STATES][ALPHABET_CHARACTERS];
+
+    public static final int UNKNOWN_SYMBOL_ERR = -1;
+    public static final int NOT_REACHED_FINAL_STATE = 0;
+    public static final int REACHED_FINAL_STATE = 1;
+
+    private int currentState;
+
+    public FA() {
+        this.currentState = STATES.q0.ordinal();
+        setTransitionsTable();
+    }
+
+    private static void setTransitionsTable() {
+        transitionTable[STATES.q0.ordinal()][INPUT._0.ordinal()] = STATES.q1.ordinal();
+        transitionTable[STATES.q0.ordinal()][INPUT._1.ordinal()] = STATES.q1.ordinal();
+        transitionTable[STATES.q1.ordinal()][INPUT._0.ordinal()] = STATES.q2.ordinal();
+        transitionTable[STATES.q1.ordinal()][INPUT._1.ordinal()] = STATES.q2.ordinal();
+        transitionTable[STATES.q2.ordinal()][INPUT._0.ordinal()] = STATES.q3.ordinal();
+        transitionTable[STATES.q2.ordinal()][INPUT._1.ordinal()] = STATES.q3.ordinal();
+        transitionTable[STATES.q3.ordinal()][INPUT._0.ordinal()] = STATES.q4.ordinal();
+        transitionTable[STATES.q3.ordinal()][INPUT._1.ordinal()] = STATES.q4.ordinal();
+        transitionTable[STATES.q4.ordinal()][INPUT._0.ordinal()] = STATES.q6.ordinal();
+        transitionTable[STATES.q4.ordinal()][INPUT._1.ordinal()] = STATES.q5.ordinal();
+        transitionTable[STATES.q5.ordinal()][INPUT._0.ordinal()] = STATES.q5.ordinal();
+        transitionTable[STATES.q5.ordinal()][INPUT._1.ordinal()] = STATES.q5.ordinal();
+        transitionTable[STATES.q6.ordinal()][INPUT._0.ordinal()] = STATES.q6.ordinal();
+        transitionTable[STATES.q6.ordinal()][INPUT._1.ordinal()] = STATES.q6.ordinal();
+    }
+
+    public int processSymbol(char current_symbol) {
+        int charPosition;
+        for (charPosition = 0; charPosition < ALPHABET_CHARACTERS; charPosition++) {
+            if (current_symbol == ALPHABET[charPosition]) break;
+        }
+        if(charPosition == ALPHABET_CHARACTERS) return UNKNOWN_SYMBOL_ERR;
+
+        this.currentState = transitionTable[this.currentState][charPosition];
+        for (STATES acceptedState : ACCEPTED_STATES) {
+            if (this.currentState == acceptedState.ordinal())
+                return REACHED_FINAL_STATE;
+        }
+        return NOT_REACHED_FINAL_STATE;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        DFA dfa = new DFA();
+        FA fa = new FA();
 
         System.out.println("Enter a binary string (only '0' and '1'):");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
-        boolean validInput = true;
+        int result = FA.UNKNOWN_SYMBOL_ERR;
         char[] charArray = input.toCharArray();
 
         for (int i = charArray.length - 1; i >= 0; i--) {
             char ch = charArray[i];
-            int result = dfa.processSymbol(ch);
-            if (result == DFA.UNKNOWN_SYMBOL_ERR) {
+            result = fa.processSymbol(ch);
+            if (result == FA.UNKNOWN_SYMBOL_ERR) {
                 System.out.println("Error: Unknown symbol encountered.");
-                validInput = false;
                 break;
             }
         }
 
-        if (validInput && dfa.isInAcceptedState()) {
+        if(result == FA.REACHED_FINAL_STATE) {
             System.out.println("Accepted");
         } else {
             System.out.println("Rejected");
